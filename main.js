9879da9c99909584170bcea6997df996e0c6de9e
jhon <thefatratunity05@gmail.com>
2020-05-28 04:43:05 -0500

    Icono o boton modo oscuro claro finalizado xd participante yisus castillo

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//obtener id retornando el objeto mediante una funcion flecha o arrow function
 let id = name => document.getElementById(name);
 
+//Evento click en el contenedor para hacer elcambio de clases / estilos
 id('container').addEventListener('click', () => {
     if (id('container').getAttribute('class') == 'container') {
         id('container').setAttribute('class', 'container1')
